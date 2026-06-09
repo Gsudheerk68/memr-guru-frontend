9dbb6eef95877d0b60da4e381a11d02ead7b0036
Gsudheerk68 <sudheersudheerkumar125@gmail.com>
2026-06-09 14:00:19 +0530

    Update Hero.jsx

--- src/components/common/Hero.jsx
+++ src/components/common/Hero.jsx
@@ -1,8 +1,7 @@
 
 import { motion } from 'framer-motion'
 
-import TemplatesPage from '/home/rguktvalley/Downloads/memer-guru/frontend/src/pages/TemplatesPage.jsx'
-import TemplatesPage from "../../pages/TemplatesPage.jsx"
+
 
 import { Link } from "react-router-dom";
 
