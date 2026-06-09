2ea278500023c6bd8aa2c99a0f0bdda290f01c82
Gsudheerk68 <sudheersudheerkumar125@gmail.com>
2026-06-09 13:40:12 +0530

    Update templateService.js

--- src/services/templateService.js
+++ src/services/templateService.js
@@ -2,11 +2,6 @@ import axios from 'axios'
 
 const API = `${import.meta.env.VITE_API_URL}/api/templates`
 
-export const getTemplates = async () => {
-  const res = await axios.get(API)
-  return res.data
-}
-
 const authHeaders = () => ({
   Authorization: `Bearer ${localStorage.getItem('token')}`
 })
@@ -17,33 +12,29 @@ export const getTemplates = async () => {
 }
 
 export const uploadTemplate = async (formData) => {
-<<<<<<< HEAD
-  const res = await axios.post(`${API}/upload`, formData, { headers: authHeaders() })
-=======
   const res = await axios.post(`${API}/upload`, formData, {
     headers: {
-      'Content-Type': 'multipart/form-data'  // ✅ add this
+      ...authHeaders(),
+      'Content-Type': 'multipart/form-data'
     }
   })
->>>>>>> f73a6c8795ef10aed46b6f4118506471efa36170
+
   return res.data
 }
 
 export const updateTemplate = async (id, data) => {
-<<<<<<< HEAD
-  const res = await axios.put(`${API}/${id}`, data, { headers: authHeaders() })
-=======
-  const res = await axios.put(`${API}/${id}`, data)
->>>>>>> f73a6c8795ef10aed46b6f4118506471efa36170
+  const res = await axios.put(`${API}/${id}`, data, {
+    headers: authHeaders()
+  })
+
   return res.data
 }
 
 export const deleteTemplate = async (id) => {
-<<<<<<< HEAD
-  const res = await axios.delete(`${API}/${id}`, { headers: authHeaders() })
-=======
-  const res = await axios.delete(`${API}/${id}`)
->>>>>>> f73a6c8795ef10aed46b6f4118506471efa36170
+  const res = await axios.delete(`${API}/${id}`, {
+    headers: authHeaders()
+  })
+
   return res.data
 }
 
