3f0f7ccf4ced9fe617c99f033ba5bab695fa3369
Gsudheerk68 <sudheersudheerkumar125@gmail.com>
2026-06-09 13:44:12 +0530

    Update memeService.js

--- src/services/memeService.js
+++ src/services/memeService.js
@@ -1,14 +1,10 @@
 import axios from 'axios'
 
-<<<<<<< HEAD
-const API = 'const API = 'https://memer-guru-version-2-o-1.onrender.com/api/memes''
+const API = `${import.meta.env.VITE_API_URL}/api/memes`
 
 const authHeaders = () => ({
   Authorization: `Bearer ${localStorage.getItem('token')}`
 })
-=======
-const API = `${import.meta.env.VITE_API_URL}/api/memes`
->>>>>>> f73a6c8795ef10aed46b6f4118506471efa36170
 
 export const getMemes = async () => {
   const res = await axios.get(API)
@@ -16,15 +12,13 @@ export const getMemes = async () => {
 }
 
 export const uploadMeme = async (formData) => {
-<<<<<<< HEAD
-  const res = await axios.post(`${API}/upload`, formData, { headers: authHeaders() })
-=======
   const res = await axios.post(`${API}/upload`, formData, {
     headers: {
-      'Content-Type': 'multipart/form-data'  // ✅ added
+      ...authHeaders(),
+      'Content-Type': 'multipart/form-data'
     }
   })
->>>>>>> f73a6c8795ef10aed46b6f4118506471efa36170
+
   return res.data
 }
 
@@ -39,10 +33,9 @@ export const viewMeme = async (id) => {
 }
 
 export const deleteMeme = async (id) => {
-<<<<<<< HEAD
-  const res = await axios.delete(`${API}/${id}`, { headers: authHeaders() })
-=======
-  const res = await axios.delete(`${API}/${id}`)
->>>>>>> f73a6c8795ef10aed46b6f4118506471efa36170
+  const res = await axios.delete(`${API}/${id}`, {
+    headers: authHeaders()
+  })
+
   return res.data
 }
